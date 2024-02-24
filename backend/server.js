5920ba84da83bf77116c09b3b47c42f97bf9dc9e
Divyanshu6797 <divyanshu6797@gmail.com>
2024-02-25 04:43:16 +0530

    second commit added axios

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("api started");
+});
+
+app.get("/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/chats/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChat = chats.find((c) => c._id === id);
+
+  res.send(singleChat);
+});
+
+const PORT = process.env.port || 5000;
+
+app.listen(5000, console.log("serve started at " + PORT));
---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Chatpage from "./Pages/Chatpage";
+
+function App() {
+  return (
+    <div className="App">
+      hemlo
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/chats" element={<Chatpage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
